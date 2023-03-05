ba8401b64a676ae623f2aed0c2030a01feb7236e
Timur <timur87@mail.ru>
2023-03-05 19:43:53 +0300

    sections portfolio added

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,3 +14,10 @@ closeElem.addEventListener("click", () => {
 overlay.addEventListener("click", () => {
   menu.classList.remove("active");
 });
+
+const counters = document.querySelectorAll(".skills__ratings-counter"),
+  lines = document.querySelectorAll(".skills__ratings-line span");
+
+counters.forEach((item, i) => {
+  lines[i].style.width = item.innerHTML;
+});
