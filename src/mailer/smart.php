ba700695a8f42cd88db1329a475b9da3cfc94895
Timur <timur87@mail.ru>
2023-06-16 02:59:42 +0300

    mailer fixed

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'timurweb05@gmail.com';                 // Наш логин
-$mail->Password = 'jjlbkdcozjvplwsq';                           // Наш пароль от ящика
+$mail->Password = 'dcrymhviuoipukcu';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
